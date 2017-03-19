64a99bc7e1b081958923200676fb9490c84dfd8d
shibamo <qinchao@hotmail.com>
2017-03-18 22:29:09 -0700

    增加流程动态用户

---
+++ Controllers/FlowDynamicUserController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using EnouFlowTemplateLib;
+
+namespace EnouFlowWebApi.Controllers
+{
+  public class FlowDynamicUserController : SharedApiController
+  {
+    // GET api/<controller>
+    public IEnumerable<FlowDynamicUser> Get()
+    {
+      return FlowTemplateDBHelper.getAllFlowDynamicUsers(true);
+    }
+  }
+}
